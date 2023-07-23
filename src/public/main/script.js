5eea91f9c25fea9ed99ab337a72b70968cae705f
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-07-23 23:52:05 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -1,10 +1,17 @@
 const textBox = document.getElementById('text-box');
 const test = document.getElementById('test')
-let colours = ['#3369e8', '#d50f25', '#eeb211', '#009925']
+let colours = {
+    blue: '#3369e8',
+    red: '#d50f25',
+    yellow: '#eeb211',
+    green: '#009925'
+}
+// let colours = ['#3369e8', '#d50f25', '#eeb211', '#009925']
+
 let wheel = []
 
-class newSegment {
-    static previousColour = '#3369e8'
+class newSector {
+    static previousColour = null
     constructor(name) {
         this.name = name
         this.colour = null
@@ -16,11 +23,25 @@ class newSegment {
     }
 
     create() {
-        if (!newSegment.previousColour) {
-            this.colour = colours[0]
+        let sectorContainer = document.getElementById('sector-container')
+        let sector = document.createElement('div')
+
+        if (!newSector.previousColour || wheel.length === 0) {
+            // console.log('hi')
+            this.colour = Object.values(colours)[0]
         } else {
-            this.colour = colours[colours.indexOf(newSegment.previousColour) + 1]
+            // console.log('hi2')
+            this.colour = Object.values(colours)[Object.values(colours).indexOf(newSector.previousColour) + 1]
         }
+        newSector.previousColour = this.colour
+        if (newSector.previousColour === Object.values(colours)[Object.values(colours).length - 1]) {
+            newSector.previousColour = null
+        }
+        
+        sector.innerText = this.name
+        sectorContainer.appendChild(sector)
+        sector.classList.add('sector')
+        sector.classList.add(`${getKeyByValue(colours, this.colour)}-sector`)
 
         wheel.push(this.name)
     }
@@ -31,7 +52,21 @@ function convertTextToArray(text) {
     return array.filter((element) => element)
 }
 
+function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+}
+
+function resetWheel() {
+    let sectorContainer = document.getElementById('sector-container')
+    while (sectorContainer.firstChild) {
+        sectorContainer.removeChild(sectorContainer.lastChild);
+    }
+    wheel = []
+}
+
 function getInput(event) {
+    resetWheel()
+
     let text;
     if (!event) {
         text = textBox.value
@@ -39,12 +74,17 @@ function getInput(event) {
         text = event.target.value
     }
     let array = convertTextToArray(text)
-    console.log(array)
+    array.forEach((element) => {
+        let sector = new newSector(element)
+        console.log(sector)
+    })
 }
 
-let idk = new newSegment('test')
-console.log(idk)
+function defaultInput() {
+    textBox.value = ['blue', 'red', 'yellow', 'green'].join('\n')
+    getInput()
+}
 
-textBox.value = ['blue', 'red', 'yellow', 'green'].join('\n')
-getInput()
+defaultInput()
 textBox.addEventListener('input', getInput, false)
+
